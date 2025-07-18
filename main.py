651b312c136af625be24bb303606f8551591724f
Babak Khorramfar <info.webschoool@gmail.com>
2025-07-19 03:06:15 +0330

    Finalize PyInstaller setup, fix QSS loading, and add styling

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
 from PySide6.QtGui import QFontDatabase
 
 if __name__ == "__main__":
 
+    # پیدا کردن مسیر صحیح فایل style.qss
+    if getattr(sys, "frozen", False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+
+    style_path = os.path.join(base_path, "resources", "styles", "style.qss")
+    font_path = os.path.join(base_path, "resources", "fonts", "Vazirmatn-Regular.ttf")
+
     app = QApplication(sys.argv)
 
-    QFontDatabase.addApplicationFont("resources/fonts/Vazirmatn-Regullar.ttf")
+    # اعمال فونت
+    QFontDatabase.addApplicationFont(font_path)
 
-    # Load QSS
-    with open("resources/styles/style.qss", "r", encoding="utf-8") as f:
-        app.setStyleSheet(f.read())
+    # اعمال استایل
+    if os.path.exists(style_path):
+        with open(style_path, "r", encoding="utf-8") as f:
+            app.setStyleSheet(f.read())
 
     window = MainWindow()
     window.show()
